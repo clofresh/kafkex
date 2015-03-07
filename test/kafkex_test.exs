661eac2dcfae1d6b35f061de432f0e9b0e2bfb48
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2015-03-07 12:19:41 -0500

    wrap test calls in try/after

--- test/kafkex_test.exs
+++ test/kafkex_test.exs
@@ -4,25 +4,31 @@ defmodule KafkexTest do
 
   test "metadata request" do
     socket = Kafkex.connect({'localhost', 19092})
-    metadata = Kafkex.metadata(socket, ["test0", "test1"], 1000, 10)
-    assert metadata == {10, [{0, "carlo-arch.localdomain", 19092}],
-                         [{0, "test0", [{0, 1, 0, [0], [0]}, {0, 0, 0, [0], [0]}]},
-                          {0, "test1", [{0, 1, 0, [0], [0]}, {0, 0, 0, [0], [0]}]}]}
-    :gen_tcp.close(socket)
+    try do
+      metadata = Kafkex.metadata(socket, ["test0", "test1"], 1000, 10)
+      assert metadata == {10, [{0, "carlo-arch.localdomain", 19092}],
+                           [{0, "test0", [{0, 1, 0, [0], [0]}, {0, 0, 0, [0], [0]}]},
+                            {0, "test1", [{0, 1, 0, [0], [0]}, {0, 0, 0, [0], [0]}]}]}
+    after
+      :gen_tcp.close(socket)
+    end
   end
 
   test "fetch request" do
     socket = Kafkex.connect({'localhost', 19092})
-    messages = Kafkex.fetch(socket, [{"test0", [{0, 0, 1024*1024}, {1, 0, 1024*1024}]},
-                                     {"test1", [{0, 0, 1024*1024}, {1, 0, 1024*1024}]}],
-                            1000, 1024, 5000, 12)
-    assert messages == {12,
-            [{"test1",
-              [{0, 0, 0, []},
-               {1, 0, 4, [{0, "qwe"}, {1, "rqw"}, {2, "543"}, {3, "4523"}]}]},
-             {"test0",
-              [{0, 0, 0, []},
-               {1, 0, 4, [{0, "test"}, {1, "test"}, {2, "adf"}, {3, "asdf"}]}]}]}
-    :gen_tcp.close(socket)
+    try do
+      messages = Kafkex.fetch(socket, [{"test0", [{0, 0, 1024*1024}, {1, 0, 1024*1024}]},
+                                       {"test1", [{0, 0, 1024*1024}, {1, 0, 1024*1024}]}],
+                              1000, 1024, 5000, 12)
+      assert messages == {12,
+              [{"test1",
+                [{0, 0, 0, []},
+                 {1, 0, 4, [{0, "qwe"}, {1, "rqw"}, {2, "543"}, {3, "4523"}]}]},
+               {"test0",
+                [{0, 0, 0, []},
+                 {1, 0, 4, [{0, "test"}, {1, "test"}, {2, "adf"}, {3, "asdf"}]}]}]}
+    after
+      :gen_tcp.close(socket)
+    end
   end
 end
